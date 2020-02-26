d79855d083f554b764c55ba280c7d799eb4f27b6
Anders Juengst <anders.juengst@gmail.com>
2020-02-26 16:44:04 -0500

    initial bg to black

--- script.js
+++ script.js
@@ -42,7 +42,7 @@ let config = {
     COLORFUL: true,
     COLOR_UPDATE_SPEED: 10,
     PAUSED: false,
-    BACK_COLOR: { r: 256, g: 256, b: 256 },
+    BACK_COLOR: { r: 0, g: 0, b: 0 },
     TRANSPARENT: false,
     BLOOM: true,
     BLOOM_ITERATIONS: 8,
